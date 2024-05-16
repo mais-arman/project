963fe2a1368bafa28a51c539ecb348b4a0b59713
Mais Arman <131681721+mais-arman@users.noreply.github.com>
2024-05-16 19:34:40 +0100

    Add files via upload

---
+++ project1/js/main.js
@@ -0,0 +1,11 @@
+var button = document.querySelector(".btn-start");
+button.addEventListener("click", function() {
+    alert("Get Started! is clicked .. Please Wait ");
+    window.location.href = "about.html";
+});
+
+var button = document.querySelector(".btn-learn");
+button.addEventListener("click", function() {
+    alert("Learn More is clicked .. Please Wait");
+    window.location.href = "Learn More.html";
+});
